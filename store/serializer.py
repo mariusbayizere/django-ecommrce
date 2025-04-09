79f218391d55d4074d3aeb6a3a50f5688c280e9b
Bayizere Marius <bayizeremarius119@gmail.com>
2025-04-09 13:55:19 +0200

    Updated-code

--- store/serializer.py
+++ store/serializer.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from .models import Product
+from decimal import Decimal
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     Title = serializers.CharField(max_length=255)
     Price = serializers.DecimalField(max_digits=6, decimal_places=2)
     Inventory = serializers.IntegerField()
+    product_tax = serializers.SerializerMethodField(method_name='get_product_tax')
+
+
+    def get_product_tax(self, product: Product):
+        return product.Price * Decimal(0.18)
